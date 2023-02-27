20aa8e99ee1b7b4402667fd509fb1dba51484f84
Aurélien SERRE <aserre@okteo.fr>
2023-02-27 15:50:19 +0100

    import assets

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div style="height: 48px;" class="fixed top-[7%] left-0 right-0 w-screen flex justify-around">
         <div>
-            <img src="src\assets\images\logo_mds.png" alt="Logo MDS">
+            <img src="..\assets\images\logo_mds.png" alt="Logo MDS">
         </div>
         <div class="content-center flex w-4/12 justify-evenly">
             <a href="#" class="text-black px-3 py-2 text-sm font-bold">Accueil</a>
